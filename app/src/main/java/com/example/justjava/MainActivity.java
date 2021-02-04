7de731f396647f3ccb295e4240f3a85268e675da
Elle Green <02elle.green@gulllakecs.org>
2021-02-04 13:11:53 -0500

    Main Activity changes

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -6,32 +6,29 @@
  * in the project's AndroidManifest.xml file.
  **/
 
-package com.example.justjava.;
+package com.example.justjava;
+
 
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
-
 /**
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
         display(1);
     }
-
     /**
      * This method displays the given quantity value on the screen.
      */
