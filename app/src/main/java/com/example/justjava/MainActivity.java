f1a6cfb6c717279bff937fa811d76c3641e3f875
Elle Green <02elle.green@gulllakecs.org>
2021-03-05 12:52:36 -0500

    Lesson 8 - use concatenation to add a string and variable together

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -40,9 +40,10 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-       // display(quantity);
+       int numberOfCoffees = quantity;
+        // display(quantity);
        // displayPrice(quantity * 5);
-        String priceMessage = "Free, did she say \"FREE\"";
+        String priceMessage = "Thank you for wanting " + numberOfCoffees + " coffees, your order will be right up!";
         displayMessage(priceMessage);
     }
     /**
